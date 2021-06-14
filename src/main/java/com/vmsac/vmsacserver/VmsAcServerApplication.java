844fc2170e3f26cc33f001ffbe2025340feeb314
Fabian Koh <koh.fabian@gmail.com>
2021-06-14 19:56:23 +0800

    Renamed files

--- src/main/java/com/vmsac/vmsacserver/VmsAcServerApplication.java
+++ src/main/java/com/vmsac/vmsacserver/VmsAcServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VmsAcBackendApplication {
+public class VmsAcServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VmsAcBackendApplication.class, args);
+		SpringApplication.run(VmsAcServerApplication.class, args);
 	}
 
 }
--- src/test/java/com/vmsac/vmsacserver/VmsAcServerApplicationTests.java
+++ src/test/java/com/vmsac/vmsacserver/VmsAcServerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VmsAcBackendApplicationTests {
+class VmsAcServerApplicationTests {
 
 	@Test
 	void contextLoads() {
