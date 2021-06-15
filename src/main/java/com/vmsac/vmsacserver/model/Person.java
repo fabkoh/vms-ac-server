4c617752808c2c461a7776bdf75f64599db78d48
Fabian Koh <koh.fabian@gmail.com>
2021-06-15 17:52:13 +0800

    Added initial Person class

--- src/main/java/com/vmsac/vmsacserver/model/Person.java
+++ src/main/java/com/vmsac/vmsacserver/model/Person.java
@@ -1,11 +1,27 @@
 package com.vmsac.vmsacserver.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table
+@Data
+@Table(name = "Person")
 public class Person {
+
+    @Id
+    @GeneratedValue
+    private Long personId;
+    private String firstName;
+    private String lastName;
+    private String lastFourDigitsOfId;
+    private String mobileNumber;
+    private String emailAdd;
 }
