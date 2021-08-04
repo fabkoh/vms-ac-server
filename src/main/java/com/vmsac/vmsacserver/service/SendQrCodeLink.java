d7a5c1a009cf4ddf93d18a8eb04c43ef4a99b189
Fabian Koh <koh.fabian@gmail.com>
2021-08-04 17:27:33 +0800

    Updated with latest QRcode SMS link

--- src/main/java/com/vmsac/vmsacserver/service/SendQrCodeLink.java
+++ src/main/java/com/vmsac/vmsacserver/service/SendQrCodeLink.java
@@ -9,10 +9,11 @@ import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.net.http.HttpConnectTimeoutException;
 
 @NoArgsConstructor
@@ -23,18 +24,32 @@ public class SendQrCodeLink {
     public void sendQrCodeLink(ScheduledVisit scheduledVisit, Visitor registeredVisitor) throws IOException, HttpClientErrorException {
         String qrCodeId = scheduledVisit.getQrCodeId();
         String visitorNumber = registeredVisitor.getMobileNumber();
-        String message = "Thanks for registering, you may click on the link below to access your QR code entry pass. "+qrCodeId;
-        String uri = "http://inthenetworld.com/sms/web_sms.php?apikey=isssecurity&mobileNumber="+visitorNumber+"&smsMessage="+message;
+        String message = "Please click on link for QR code: http://ec2-13-212-193-249.ap-southeast-1.compute.amazonaws.com:5000/visitor/qrcode?qrCode="+qrCodeId;
 
-        URL url = new URL(uri);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("POST");
-        int status = con.getResponseCode();
+        String urlAddress = "http://inthenetworld.com/sms/send_sms.php";
+
+        URL url = new URL(urlAddress);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setDoOutput(true);
+        OutputStream opstream = httpURLConnection.getOutputStream();
+        BufferedWriter bfwriter = new BufferedWriter(new OutputStreamWriter(opstream, "UTF-8"));
+
+        String dstring = URLEncoder.encode("apikey", "UTF-8")+"="+URLEncoder.encode("isssecurity", "UTF-8")+"&"+URLEncoder.encode("mobileNumber", "UTF-8")+"="+URLEncoder.encode(visitorNumber, "UTF-8") +"&"+URLEncoder.encode("smsMessage", "UTF-8")+"="+URLEncoder.encode(message, "UTF-8");
+
+        bfwriter.write(dstring);
+        bfwriter.flush();
+        bfwriter.close();
+        opstream.close();
+        InputStream ipstream = httpURLConnection.getInputStream();
+        ipstream.close();
+        httpURLConnection.disconnect();
 
         System.out.println(qrCodeId);
         System.out.println(visitorNumber);
         System.out.println(message);
-        System.out.println(uri);
-        System.out.println(status);
+        System.out.println(urlAddress);
+        //System.out.println(status);
     }
 }
