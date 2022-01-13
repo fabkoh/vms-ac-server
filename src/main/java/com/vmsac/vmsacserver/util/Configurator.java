22f55412d60a73635207c1be4f269adc50699d22
ongweihong-isssecurity <weihong.ong@isssecurity.sg>
2022-01-13 17:55:49 +0800

    updated schema, added jasypt encryption for application.properties

---
+++ src/main/java/com/vmsac/vmsacserver/util/Configurator.java
@@ -0,0 +1,10 @@
+package com.vmsac.vmsacserver.util;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.context.annotation.Configuration;;
+
+@Configuration
+@EnableEncryptableProperties
+public class Configurator {
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into visitor values ('96653993','Fabian','Koh','470A','koh.fabian@gmail.com');
+insert into visitor values ('78945621','Dan','Tham','456A','daniel.tham@gmail.com');
+insert into visitor values ('12345678', 'John','Smith','321Z','john.smith@gmail.com');
+insert into visitor values ('48997562','Jane','Tan','586G','jane.tan@gmail.com');
