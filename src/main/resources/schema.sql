3052f1a5e1ae3d38397e4d4569a2171a3012859c
Fabian Koh <koh.fabian@gmail.com>
2022-01-13 22:27:17 +0800

    Updated with latest production properties file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Visitor (
+CREATE TABLE IF NOT EXISTS Visitor (
     idNumber VARCHAR(128) NOT NULL,
     firstName VARCHAR(128),
     lastName VARCHAR(128),
@@ -6,9 +6,10 @@ CREATE TABLE Visitor (
     emailAdd VARCHAR(128),
     PRIMARY KEY (idNumber)
 );
-CREATE TABLE ScheduledVisit (
+
+CREATE TABLE IF NOT EXISTS ScheduledVisit (
     scheduledVisitId SERIAL NOT NULL,
-    idNumber VARCHAR(128),
+    idNumber VARCHAR(128) REFERENCES Visitor (idNumber),
     startDateOfVisit DATE,
     endDateOfVisit DATE,
     qrCodeId VARCHAR(128),
@@ -17,3 +18,46 @@ CREATE TABLE ScheduledVisit (
     raisedBy BIGINT,
     PRIMARY KEY (scheduledVisitId)
 );
+
+CREATE TABLE IF NOT EXISTS AccessGroups (
+  accessGroupId SERIAL NOT NULL UNIQUE,
+  accessGroupName VARCHAR(255) NOT NULL UNIQUE,
+  accessGroupDesc TEXT,
+  deleted BOOLEAN,
+  PRIMARY KEY (accessGroupId)
+);
+
+CREATE TABLE IF NOT EXISTS Persons (
+  personId SERIAL NOT NULL UNIQUE,
+  personFirstName VARCHAR(128) NOT NULL,
+  personLastName VARCHAR(128) NOT NULL,
+  personUID VARCHAR(128) NOT NULL UNIQUE,
+  personMobileNumber VARCHAR(128),
+  personEmail VARCHAR(128),
+  accessGroupId INT REFERENCES AccessGroups (accessGroupId),
+  deleted BOOLEAN NOT NULL,
+  PRIMARY KEY (personId)
+);
+
+CREATE TABLE IF NOT EXISTS OrgGroups (
+  orgGroupId SERIAL NOT NULL UNIQUE,
+  orgGroupName VARCHAR(255) NOT NULL UNIQUE,
+  orgGroupDesc TEXT,
+  deleted BOOLEAN NOT NULL,
+  PRIMARY KEY (orgGroupId)
+);
+
+CREATE TABLE IF NOT EXISTS PersonOrgGroupNtoN (
+  personId INT REFERENCES Persons (personId),
+  orgGroupId INT REFERENCES OrgGroups (orgGroupId),
+  deleted BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS CredentialType(
+  credTypeId SERIAL NOT NULL UNIQUE,
+  credTypeName VARCHAR(255) NOT NULL UNIQUE,
+  credTypeDesc VARCHAR(255),
+  deleted BOOLEAN,
+  PRIMARY KEY (credTypeId)
+);
+
