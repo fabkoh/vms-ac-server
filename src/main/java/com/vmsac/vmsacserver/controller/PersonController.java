517b0d740df57acb2c321f9c31b10ba1c41bf515
ong-wei-hong <weihong.ong@outlook.com>
2022-01-19 10:34:55 +0800

    merged bugfix/uid-bug

--- src/main/java/com/vmsac/vmsacserver/controller/PersonController.java
+++ src/main/java/com/vmsac/vmsacserver/controller/PersonController.java
@@ -1,5 +1,9 @@
 package com.vmsac.vmsacserver.controller;
 
+<<<<<<< Updated upstream
+=======
+import com.vmsac.vmsacserver.model.CreatePersonDto;
+>>>>>>> Stashed changes
 import com.vmsac.vmsacserver.model.Person;
 import com.vmsac.vmsacserver.model.PersonDto;
 import com.vmsac.vmsacserver.service.PersonService;
@@ -11,7 +15,14 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+<<<<<<< Updated upstream
 import java.util.*;
+=======
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+>>>>>>> Stashed changes
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -23,68 +34,82 @@ public class PersonController {
 
     @GetMapping("/persons")
     public List<PersonDto> getPersons() {
-        return personService.findAll();
+        return personService.findAllNotDeleted();
     }
 
     // checks if uid is in use
     @PostMapping(path = "/person")
     public ResponseEntity<?> createPerson(
-            @Valid @RequestBody PersonDto newPerson) {
-        if (personService.uidInUse(newPerson.getPersonUid())) {
+            @Valid @RequestBody CreatePersonDto newPersonDto) {
+
+        if(newPersonDto.getPersonUid() == null || newPersonDto.getPersonUid().isBlank()) {
+            newPersonDto.setPersonUid(personService.generateUid());
+        }else if (personService.uidInUse(newPersonDto.getPersonUid())) {
             Map<String, String> errors = new HashMap<>();
-            errors.put("personUid", "Person UID in use");
+            errors.put("personUid", "Person UID " +
+                    newPersonDto.getPersonUid() + " in use");
             return new ResponseEntity<>(errors, HttpStatus.CONFLICT);
         }
-        return new ResponseEntity<>(personService.save(newPerson, false),
+
+        return new ResponseEntity<>(personService.createNotDeleted(newPersonDto),
                 HttpStatus.CREATED);
     }
 
-    // checks if person exists by id, id in path and body is the same and
-    // if uid is used by another person
-    @PutMapping(path = "/person/{personId}")
-    public ResponseEntity<?> updatePerson(@PathVariable("personId")
-                                                      Long personId,
-                                          @Valid @RequestBody
-                                                  PersonDto newPerson) {
-        boolean invalid = false;
-        Map<String, String> errors = new HashMap<>();
+    // checks if uid is blank, id in database and uid used by another person
+    // TODO: checking of uid and is in database calls database search twice,
+    //  implement both checks at the same time
+    @PutMapping(path = "/person")
+    public ResponseEntity<?> updatePerson(
+            @Valid @RequestBody PersonDto updatePersonDto) {
 
-        if(!personService.exists(personId, false)) {
-            invalid = true;
-            errors.put("personId", "Person with ID " + personId + " does not exist");
+        if(!personService.idInUse(updatePersonDto.getPersonId())) {
+            Map<String, String> errors = new HashMap<>();
+            errors.put("personId", "Person with Id " +
+                    updatePersonDto.getPersonId() + " does not exist");
+            return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
         }
 
-        if(!Objects.equals(personId, newPerson.getPersonId())) {
-            invalid = true;
-            errors.put("personId", "PersonId cannot be changed");
+        if(personService.uidInUse(
+                updatePersonDto.getPersonUid(), updatePersonDto.getPersonId())) {
+            Map<String, String> errors = new HashMap<>();
+            errors.put("personUid", "Person UID " +
+                    updatePersonDto.getPersonUid() + " is already in use");
+            return new ResponseEntity<>(errors, HttpStatus.CONFLICT);
         }
 
-        if(personService.uidInUse(newPerson.getPersonUid(), personId)) {
-            invalid = true;
-            errors.put("personUid", "UID " + newPerson.getPersonUid() + " is already in use");
-        }
-
-        if(invalid) {
-            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-        }
-
-        return ResponseEntity.ok(personService.save(newPerson, false));
+        return ResponseEntity.ok(personService.save(updatePersonDto, false));
     }
 
     @DeleteMapping(path = "/person/{personId}")
+<<<<<<< Updated upstream
     public ResponseEntity<?> deletePerson(@PathVariable("personId") Long personId) {
         Optional<Person> optionalPerson = personService.findByIdAndDeleted(personId, false);
+=======
+    public ResponseEntity<?> deletePerson(
+            @PathVariable("personId") Long personId) {
+
+        Optional<Person> optionalPerson =
+                personService.findByIdInUse(personId);
+>>>>>>> Stashed changes
 
         if(optionalPerson.isEmpty()) {
             Map<String, String> errors = new HashMap<>();
-            errors.put("personId", "Person with ID " + personId + " does not exist");
-            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+            errors.put("personId",
+                    "Person with ID " + personId + " does not exist");
+            return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
         }
+<<<<<<< Updated upstream
         personService.delete(optionalPerson.get());
+=======
+
+        Person deletePerson = optionalPerson.get();
+        deletePerson.setDeleted(true);
+        personService.save(deletePerson);
+>>>>>>> Stashed changes
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleValidationExceptions(
             MethodArgumentNotValidException ex) {
