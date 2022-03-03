90a147d9ea5fee039e1b8b36c7a074b7a2263a25
jieelinnchew <jielin.chew@isssecurity.sg>
2022-03-03 10:02:51 +0800

    Created Entrance Model

---
+++ src/main/java/com/vmsac/vmsacserver/model/EntranceOnlyDto.java
@@ -0,0 +1,30 @@
+package com.vmsac.vmsacserver.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EntranceOnlyDto {
+
+    @Positive(message = "entranceId must be greater than 1")
+    @NotNull(message = "entranceId must not be empty")
+    private Long entranceId;
+
+    @NotNull(message = "EntranceName must not be empty")
+    private String entranceName;
+
+    private String entranceDesc;
+
+    private Boolean isEnabled;
+
+    public Entrance toEntrance(Boolean deleted){
+        return new Entrance(entranceId,entranceName,entranceDesc, isEnabled, deleted);
+    }
+}
