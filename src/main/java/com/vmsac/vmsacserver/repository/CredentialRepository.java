5868cb7298ae5733c354efa6f0c0affebb2018f4
ong-wei-hong <weihong.ong@outlook.com>
2022-04-08 13:47:24 +0800

    completed delete and update credential paths

--- src/main/java/com/vmsac/vmsacserver/repository/CredentialRepository.java
+++ src/main/java/com/vmsac/vmsacserver/repository/CredentialRepository.java
@@ -3,10 +3,14 @@ package com.vmsac.vmsacserver.repository;
 import com.vmsac.vmsacserver.model.credential.Credential;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CredentialRepository extends JpaRepository<Credential, Long> {
 
     Optional<Credential> findByCredTypeCredTypeIdAndCredUidAndDeletedFalse(Long credTypeId, String credUid);
 
+    List<Credential> findAllByPersonPersonIdAndDeletedFalse(Long personId);
+
+    Optional<Credential> findByCredIdAndDeletedFalse(Long credId);
 }
