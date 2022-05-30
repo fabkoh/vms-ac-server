494fdd71d432060cc1a6c078049e7737480abfa7
BryanPSK <48677467+BryanPSK@users.noreply.github.com>
2022-05-30 14:20:27 +0800

    Added schedule validations

--- src/main/java/com/vmsac/vmsacserver/repository/AuthDeviceRepository.java
+++ src/main/java/com/vmsac/vmsacserver/repository/AuthDeviceRepository.java
@@ -19,6 +19,8 @@ public interface AuthDeviceRepository extends JpaRepository<AuthDevice,Long> {
 
     Optional<AuthDevice> findByAuthDeviceId(Long authDeviceId);
 
+    Optional<AuthDevice> findByAuthMethodSchedules_AuthMethodScheduleId(Long authMethodScheduleId);
+
 
 
 }
