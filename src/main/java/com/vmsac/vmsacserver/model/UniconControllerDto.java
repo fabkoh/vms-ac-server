780b157e94a8e1e158c55174ab6ef6f0d258c0b4
Alissa <alissayarmantho1@gmail.com>
2022-08-12 10:58:41 +0800

    [VS-464] Add last sync to controller

--- src/main/java/com/vmsac/vmsacserver/model/UniconControllerDto.java
+++ src/main/java/com/vmsac/vmsacserver/model/UniconControllerDto.java
@@ -28,15 +28,15 @@ public class UniconControllerDto {
     @NonNull
     private String controllerSerialNo;
 
-    public Controller toCreateController(String controllerName, LocalDateTime lastOnline,Boolean mastercontroller, LocalDateTime created, String pinAssignmentConfig, String settingsConfig, Boolean deleted){
+    public Controller toCreateController(String controllerName, LocalDateTime lastOnline, LocalDateTime lastSync, Boolean mastercontroller, LocalDateTime created, String pinAssignmentConfig, String settingsConfig, Boolean deleted){
         return new Controller(null,controllerIPStatic,controllerName,controllerIP,null,
-                controllerMAC,controllerSerialNo,lastOnline,created,mastercontroller,pinAssignmentConfig,
+                controllerMAC,controllerSerialNo,lastOnline, lastSync, created,mastercontroller,pinAssignmentConfig,
                 settingsConfig,deleted,null, null);
     }
 
     public Controller toController(){
         return new Controller(controllerId,controllerIPStatic,null,controllerIP,null,
-                controllerMAC,controllerSerialNo,null,null,null,
+                controllerMAC,controllerSerialNo,null, null, null,null,
                 null,null,false,null, null);
     }
 //    include auth device to update status ( online/ offline )
