24ca47f948402a9f5cb864559eceb41df8ee1da8
futuresea-dev <futuresea.dev@gmail.com>
2022-08-30 14:31:00 -0300

    added UserRepository class

---
+++ src/main/java/com/vmsac/vmsacserver/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.vmsac.vmsacserver.repository;
+
+import com.vmsac.vmsacserver.model.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByUsername(String username);
+  Boolean existsByUsername(String username);
+  Boolean existsByEmail(String email);
+}
