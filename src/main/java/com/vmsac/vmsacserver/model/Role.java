8c47c465d62fbad12c04c863bbf3cba138acc6bc
WFWA TECH <tusharinani93@gmail.com>
2022-10-14 21:43:37 +0530

    Created: Role Model

---
+++ src/main/java/com/vmsac/vmsacserver/model/Role.java
@@ -0,0 +1,39 @@
+package com.vmsac.vmsacserver.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 20)
+    @Enumerated(EnumType.STRING)
+    private ERole name;
+
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
+}
