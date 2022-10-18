be37e8406d09d7fce883ea436728c5e8fdf98a8a
WFWA TECH <tusharinani93@gmail.com>
2022-10-18 13:57:46 +0530

    Fix: h2-console page not rendering disabled: frameOptions in spring security

--- src/main/java/com/vmsac/vmsacserver/security/WebSecurityConfig.java
+++ src/main/java/com/vmsac/vmsacserver/security/WebSecurityConfig.java
@@ -53,6 +53,7 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
