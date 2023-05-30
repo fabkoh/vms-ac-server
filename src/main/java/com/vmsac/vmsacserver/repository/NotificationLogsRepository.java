cd12b583b8513ebd9bda5caf0963e5612e95e486
bickybong <bickybong@gmail.com>
2023-05-30 16:00:21 +0800

    fixed notif search bug

--- src/main/java/com/vmsac/vmsacserver/repository/NotificationLogsRepository.java
+++ src/main/java/com/vmsac/vmsacserver/repository/NotificationLogsRepository.java
@@ -5,6 +5,8 @@ import com.vmsac.vmsacserver.model.notification.NotificationLogs;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.sql.Timestamp;
 
 import java.util.List;
@@ -27,10 +29,14 @@ public interface NotificationLogsRepository extends JpaRepository<NotificationLo
             "to_timestamp(timesent, 'mm-dd-yyyy HH24:MI:SS') <= :end " +
             "order by timesent desc", nativeQuery = true)
     List<NotificationLogs> findByTimeEnd(Timestamp end, Pageable pageable);
-    @Query(value = "select * from notificationlogs n where " +
-            "to_timestamp(timesent, 'mm-dd-yyyy HH24:MI:SS') between :start and :end " +
-            "order by timesent desc", nativeQuery = true)
-    List<NotificationLogs> findByTime(Timestamp start, Timestamp end, Pageable pageable);
+
+//    @Query(value = "select * from notificationlogs n where " +
+//            "AND to_timestamp(n.timesent, 'mm-dd-yyyy HH24:MI:SS') between :start and :end " +
+//            "order by timesent desc", nativeQuery = true)
+//    List<NotificationLogs> findByTime(Timestamp start, Timestamp end, Pageable pageable);
+
+    @Query(value = "SELECT * FROM notificationlogs n WHERE TO_TIMESTAMP(n.timesent, 'MM-DD-YYYY HH24:MI:SS') BETWEEN :start AND :end ORDER BY n.timesent DESC", nativeQuery = true)
+    List<NotificationLogs> findByTime(@Param("start") Timestamp start, @Param("end") Timestamp end, Pageable pageable);
 
     List<NotificationLogs> findByOrderByTimeSentDesc(Pageable pageable);
 
