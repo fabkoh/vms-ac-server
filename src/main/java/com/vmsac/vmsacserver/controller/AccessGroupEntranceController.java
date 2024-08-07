2c4d5f9e1a3697ecce9f2b8199e2bd9865a16330
junnengsoo <85351906+junnengsoo@users.noreply.github.com>
2024-08-07 15:45:31 +0800

    Revert jpa settings

--- src/main/java/com/vmsac/vmsacserver/controller/AccessGroupEntranceController.java
+++ src/main/java/com/vmsac/vmsacserver/controller/AccessGroupEntranceController.java
@@ -15,6 +15,7 @@ import java.util.Objects;
 @RequestMapping("/api")
 public class AccessGroupEntranceController {
 
+
     @Autowired
     private AccessGroupEntranceService accessGroupEntranceService;
 
