683feb091ba0fc8df847ce554cb4da84aa128090
junnengsoo <soojunneng01@gmail.com>
2026-03-26 12:33:36 +0800

    add cred type method

--- src/main/java/com/vmsac/vmsacserver/repository/CredTypeRepository.java
+++ src/main/java/com/vmsac/vmsacserver/repository/CredTypeRepository.java
@@ -4,7 +4,10 @@ import com.vmsac.vmsacserver.model.credentialtype.CredentialType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CredTypeRepository extends JpaRepository<CredentialType, Long> {
     List<CredentialType> findAll();
+
+    Optional<CredentialType> findByCredTypeNameAndDeletedFalse(String credTypeName);
 }
